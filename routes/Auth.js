4026bdf95d9d19342a6676f89c7645984728e44c
VikasMeshram2708 <meshramvikas744@gmail.com>
2023-03-08 17:09:02 +0530

    :sparkles: created login route.

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const schema = require('../models/auth');
-const Joi = require('joi');
 const monk = require('monk');
 const db = monk(process.env.MONGO_URI);
 const users = db.get('users');
@@ -31,6 +30,33 @@ router.post('/signUp', async (req, res) => {
     }
     return res.status(422).json({
       message: 'try, to sign up with proper credentials!',
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Some Internal Server Error!',
+      error: error.message,
+    });
+  }
+});
+
+router.post('/signIn', async (req, res) => {
+  try {
+    // find the user email
+    const User = await users.findOne({ email: req.body.email });
+    if (User) {
+      // compare the passowrd
+      const isValidKey = await bcrypt.compare(req.body.password, User.password);
+      if (!isValidKey) {
+        return res.status(422).json({
+          message: 'Invalid credential proveded',
+        });
+      }
+      return res.status(201).json({
+        message: 'user Logged in Successfully!',
+      });
+    }
+    return res.status(422).json({
+      message: 'Invalid credentails provided!',
     });
   } catch (error) {
     return res.status(500).json({
